1deefe5b79557072166fddaf379d5dcaa946ccab
Kenny Cao <kennyxcao@gmail.com>
2018-04-07 21:18:46 -0700

    Update completed course source code

--- learn-redux/client/data/config.js
+++ learn-redux/client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = '4a98d038a35f459c8910f511b9bd4406';
+const sentry_app = '1162565';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
--- learn-redux/client/data/posts.js
+++ learn-redux/client/data/posts.js
@@ -4,168 +4,168 @@ const posts = [
       "caption":"Lunch #hamont",
       "likes":56,
       "id":"1161022966406956503",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12552326_495932673919321_1443393332_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqdEr-QTwUdmkS4DhmOlFFY3xA4aqoZ-0NsaP5mrF7104v77Ba"
    },
    {
       "code":"BAcJeJrQca9",
       "caption":"Snow! ⛄️🌨❄️ #lifewithsnickers",
       "likes":59,
       "id":"1160844458347054781",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg"
+      "display_src":"https://i.pinimg.com/236x/6e/c0/f7/6ec0f776fa12bdc9398c174f047e5457--instagram-photo-ideas-rome-italy.jpg"
    },
    {
       "code":"BAF_KY4wcRY",
       "caption":"Cleaned my office and mounted my recording gear overhead. Stoked for 2016!",
       "likes":79,
       "id":"1154606670337393752",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/e35/923995_1704188643150533_1383710275_n.jpg"
+      "display_src":"https://i.pinimg.com/originals/12/69/42/1269422c38028729c5afe7f89151cd37.png"
    },
    {
       "code":"BAPIPRjQce9",
       "caption":"Making baby pancakes for one early rising baby. ☕️🍴",
       "likes":47,
       "id":"1157179863266871229",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12407480_1654828594805097_152207166_n.jpg"
+      "display_src":"https://capcay.net/wp-content/uploads/2018/02/1518736478_angle-foto-saat-traveling-yang-kece-untuk-di-upload-di-instagram-menurut-alexander-thian"
    },
    {
       "code":"-hZh6IQcfN",
       "caption":"New Stickers just came in. I'll do another mailing in a few weeks if you want some. #javascript",
       "likes":66,
       "id":"1126293663140399053",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/11875511_1562439187344831_813588280_n.jpg"
+      "display_src":"https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2016/11/21/17/instagram-tips.jpg"
    },
    {
       "code":"-B3eiIwcYV",
       "caption":"Tacos for breakfast. I love you Austin. 🇺🇸",
       "likes":33,
       "id":"1117418173361145365",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e35/11917950_927755223968499_1198055371_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSakBOSJWTqz-Y93cnwHSgAkqY7Iat-H21FDOaq8GWG55_ZSvXS"
    },
    {
       "code":"BAhvZrRwcfu",
       "caption":"Tried poke for the first time at @pokehbar. Delicious! It's like a bowl of sushi",
       "likes":30,
       "id":"1162418651480049646",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e35/12501993_1504179163220771_2060674913_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6zzvah9NCxKGBXAAp5DbCUUvc9qhbmNM1YSXdvK8z2LBg9Kmx"
    },
    {
       "code":"BAAJqbOQcW5",
       "caption":"Brunchin'",
       "likes":40,
       "id":"1152964002473690553",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/1516572_445736812276082_2116173059_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz_FE63I-h9VddOnMMEIOYbX7vuXDx-K-79i9XT_wI6X13ovQK"
    },
    {
       "code":"_4jHytwcUA",
       "caption":"2015 can be summed up with one baby and a many lines of code. And sometimes a coding baby. 👶🏼⌨",
       "likes":62,
       "id":"1150824171912152320",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e35/10723795_1149927178351091_1859033096_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK3mCdK9bXfqZpcQPuzb8LbgrYgOZFMomptEmKJWlFM-dOdUjL"
    },
    {
       "code":"_zbaOlQcbn",
       "caption":"Lekker Chocoladeletter",
       "likes":52,
       "id":"1149382879529256679",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12346073_1035047523184672_768982339_n.jpg"
+      "display_src":"https://images.summitmedia-digital.com/preview/images/articles/2016/03/30/bloggers/nm_blogig.jpg"
    },
    {
       "code":"_rmvQfQce8",
       "caption":"Just discovered the #hamont farmers market has a new ramen place! 🍜",
       "likes":35,
       "id":"1147180903383025596",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12331739_1671776806423597_995664526_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUySuXSkENlqGrYtV-E57R-DNCTpuQ8l-NaVoBGE84Meb0yB3"
    },
    {
       "code":"_ep9kiQcVy",
       "caption":"⛄️",
       "likes":64,
       "id":"1143535906423162226",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12354078_447337935474115_1484398925_n.jpg"
+      "display_src":"https://i.pinimg.com/originals/1a/b9/bc/1ab9bcba0144d9176259591f308e5406.jpg"
    },
    {
       "code":"_XpJcrwcSn",
       "caption":"6 page spread on flexbox in this months netmag!",
       "likes":74,
       "id":"1141561999742846119",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12362588_1688046211438811_1395882545_n.jpg"
+      "display_src":"https://pbs.twimg.com/media/BvIyb4eCcAAKdlA.jpg"
    },
    {
       "code":"_KnU7MwceA",
       "caption":"Hanging out in my office waiting for 5:00 beers to come around.",
       "likes":54,
       "id":"1137894817632733056",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12301208_1533749386944985_1334730917_n.jpg"
+      "display_src":"https://cdn-starbucks.netdna-ssl.com/uploads/images/_framed/d701uyUS-596-599.JPG"
    },
    {
       "code":"_HMejJQcY5",
       "caption":"Today I learned that a long pull espresso is called a 'lungo'",
       "likes":18,
       "id":"1136932306813044281",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12357319_493317964181479_310198908_n.jpg"
+      "display_src":"https://pbs.twimg.com/media/DYIdXhoV4AAiYqc.jpg"
    },
    {
       "code":"_Fq2zmwcaz",
       "caption":"Awesome hand lettered gift from @eunibae and the HackerYou crew.",
       "likes":48,
       "id":"1136502965197194931",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12317458_1692845870986430_331905833_n.jpg"
+      "display_src":"http://i.dailymail.co.uk/i/pix/2017/09/03/23/43E1B14200000578-4849300-image-m-18_1504478012599.jpg"
    },
    {
       "code":"_A2r0aQcfD",
       "caption":"Some serious hardware meet JavaScript hacks going down this week at hackeryou. Excited for demo day!",
       "likes":57,
       "id":"1135147611821557699",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12276809_750065668431999_184252508_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIM9KDN24a33lCKOeNoc3B1iS6kSLsjJ5UbnniYNGKUjwh83Vq"
    },
    {
       "code":"-1rhFawccs",
       "caption":"Some major audio upgrades coming to my next videos 😍",
       "likes":39,
       "id":"1132002270913873708",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12331333_1650987978502155_1162510634_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv36hERSjL1kI5BGL5_wMIjN03BuiolYBgXtXCLGHaxbrFMwGi"
    },
    {
       "code":"-pjx-gQcVi",
       "caption":"My baby and me. Thanks to @bearandsparrow for this one.",
       "likes":81,
       "id":"1128590547628442978",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12298962_863814057068027_460827278_n.jpg"
+      "display_src":"https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2017/09/offthepath.jpg"
    },
    {
       "code":"-oTZ0zQcWt",
       "caption":"It's too early. Send coffee.",
       "likes":81,
       "id":"1128237044221461933",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12328347_990748230999662_1512917342_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLD1yiHHi-BeXXn1AGD6Hv4mFrzi4D3pAc6dqQXc6dM0uYfEmi"
    },
    {
       "code":"-mxKQoQcQh",
       "caption":"They both have figured it out. #lifewithsnickers",
       "likes":47,
       "id":"1127804966031967265",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtp1/t51.2885-15/e35/12256736_1758525004381641_1136705181_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fwDJLHp27O2rVkXPfUReHrsapXVDt0uMf0-v1tr6EfxMlkWL"
    },
    {
       "code":"-fasqlQceO",
       "caption":"Kaitlin decorated the house for the Christmas. So gezellig! #casabos",
       "likes":46,
       "id":"1125735850454402958",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/12277581_1028556737218368_1184190781_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrmSJuuwOG40oAZv5ybez106LCDnLc4jy5tp5LGYAIz9BlqyXP"
    },
    {
       "code":"-VBgtGQcSf",
       "caption":"Trying the new Hamilton Brewery beer. Big fan.",
       "likes":27,
       "id":"1122810327591928991",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12224456_175248682823294_1558707223_n.jpg"
+      "display_src":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTLE6IksYQbDitxSpvOicqgL7FyhlDF9xDL9Ww3NDzNf2w1OFT"
    },
    {
       "code":"-FpTyHQcau",
       "caption":"I'm in Austin for a conference and doing some training. Enjoying some local brew with my baby.",
       "likes":82,
       "id":"1118481761857291950",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/11326072_550275398458202_1726754023_n.jpg"
+      "display_src":"https://neilpatel-qvjnwj7eutn3.netdna-ssl.com/wp-content/uploads/2017/01/instagram.jpg"
    }
 ];
 
